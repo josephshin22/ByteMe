1a3373b337a975a5709c3a59aa1c975a8bcd1ecc
josephshin22 <121205958+josephshin22@users.noreply.github.com>
2025-03-13 09:41:55 -0400

    Add password/login verification

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,16 +1,35 @@
 public class Student {
 
-    String username;
-    int studentID;
-    String password;
-    String[] major;
-    String[] minor;
-    String gradYear;
-    String gradMonth;
-    Course[] takenCourses;
-    Schedule[] schedules;
+    private String username;
+    private int studentID;
+    private String password;
+    private String[] major;
+    private String[] minor;
+    private String gradYear;
+    private String gradMonth;
+    private Course[] takenCourses;
+    private Schedule[] schedules;
 
     public Student() {
+    }
 
+    public boolean verifyPassword(String passwordAttempt) {
+        return passwordAttempt.equals(password);
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword(String password) {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
